e70bcca3c35f3e67341fe1c6104dd46ead36b003
francis victoria ogwa <101930191+vicogwa@users.noreply.github.com>
2025-12-02 18:29:26 +0100

    Add Terraform backend configuration for S3

---
+++ infra/terraform/backend.tf
@@ -0,0 +1,24 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-1764405165"
+    key            = "microservices/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+}
