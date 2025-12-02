3ac2d38a65c2930a1d7929bd5c933f339cc0b955
francis victoria ogwa <101930191+vicogwa@users.noreply.github.com>
2025-12-02 20:39:52 +0100

    Enhance variable definitions in Terraform configuration
    Added descriptions and types for existing variables and introduced new variables for domain name, SSH private key path, email alerts, GitHub repo URL, and allowed SSH CIDR.

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,17 +1,44 @@
 variable "aws_region" {
-  default = "us-east-1"
-}
-
-variable "ami_id" {
-  description = "AMI ID for Ubuntu 22.04"
-  default     = "ami-0a695f0d95cefc163"
+  description = "AWS region for resources"
+  type        = string
+  default     = "us-east-1"  
 }
 
 variable "instance_type" {
-  default = "t2.medium"
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.medium"  
 }
 
 variable "key_name" {
-  description = "SSH key pair name"
+  description = "AWS SSH key pair name"
+  type        = string
+}
 
+variable "domain_name" {
+  description = "Domain name for the application"
+  type        = string
+  default     = "mytodos.mooo.com"
+}
+
+variable "ssh_private_key_path" {
+  description = "Path to SSH private key for Ansible connection"
+  type        = string
+  default     = "~/.ssh/id_rsa"
+}
+
+variable "email_for_alerts" {
+  description = "Email address for drift detection alerts"
+  type        = string
+}
+
+variable "github_repo_url" {
+  description = "GitHub repository URL to clone"
+  type        = string
+}
+
+variable "allowed_ssh_cidr" {
+  description = "CIDR block allowed to SSH into the instance"
+  type        = string
+  default     = "0.0.0.0/0"  
 }
