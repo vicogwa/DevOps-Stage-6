f663968c6d43ca89bc9a67b3159862894236ace4
francis victoria ogwa <101930191+vicogwa@users.noreply.github.com>
2025-12-02 21:45:40 +0100

    Update DynamoDB table name for state locking

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -22,6 +22,6 @@ terraform {
     key            = "todo-app/terraform.tfstate"
     region         = "us-east-1"  
     encrypt        = true
-    dynamodb_table = "terraform-state-lock"  
+    dynamodb_table = "vicogwa-state-lock"  
   }
 }
