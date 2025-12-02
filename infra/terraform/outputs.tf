f781989b4d066b71fb4fb73e29d007eee3414e61
francis victoria ogwa <101930191+vicogwa@users.noreply.github.com>
2025-12-02 20:23:49 +0100

    Add outputs for EC2 instance and application settings
    Added outputs for EC2 instance details and application configuration.

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,7 +1,29 @@
-output "server_ip" {
-  value = aws_instance.app_server.public_ip
+output "instance_id" {
+  description = "EC2 instance ID"
+  value       = aws_instance.todo_app_server.id
 }
 
-output "server_id" {
-  value = aws_instance.app_server.id
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.todo_app_server.public_ip
+}
+
+output "instance_public_dns" {
+  description = "Public DNS of the EC2 instance"
+  value       = aws_instance.todo_app_server.public_dns
+}
+
+output "security_group_id" {
+  description = "Security group ID"
+  value       = aws_security_group.todo_app_sg.id
+}
+
+output "domain_name" {
+  description = "Application domain name"
+  value       = var.domain_name
+}
+
+output "application_url" {
+  description = "Application URL"
+  value       = "https://${var.domain_name}"
 }
